3a5100c7d3e5d9266b4d592fc38453ced1f28955
sitthinon_s <sitthinon.s@lmwn.com>
2021-01-09 19:06:52 +0700

    chore: ✨ initial spring boot

---
+++ auth/src/main/java/com/devgeta/auth/AuthApplication.java
@@ -0,0 +1,13 @@
+package com.devgeta.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApplication.class, args);
+    }
+
+}
---
+++ resource/src/test/java/com/devgeta/resource/ResourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devgeta.resource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
