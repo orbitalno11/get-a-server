58b6e69274930c9c53767116c485b0aa4ff211fe
sitthinon_s <sitthinon.s@lmwn.com>
2021-01-09 18:58:10 +0700

    chore:✨ re initialize

--- models/main.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-from models.rfm import RFM
-
-
-data = pd.read_csv('data/rfm_data1.csv')
-rfm = RFM(data)
-rfm.calculate_rfm()
--- utils/date_preparation.py
+++
@@ -1,9 +0,0 @@
-import datetime
-
-
-class DatePreparation:
-
-    @staticmethod
-    def string_to_date(date_string: str, date_format: str = '%d/%m/%Y') -> datetime:
-        date_object = datetime.datetime.strptime(date_string, date_format)
-        return date_object
