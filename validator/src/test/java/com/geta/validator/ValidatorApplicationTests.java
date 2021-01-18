34f6ea90bf0622473df10c1c1e8d13ea9a7b93b5
sitthinon_s <sitthinon.s@lmwn.com>
2021-01-18 23:30:53 +0700

    feat: ✨ add validator module

---
+++ validator/src/test/java/com/geta/validator/ValidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geta.validator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
