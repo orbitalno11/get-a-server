24abaed1c2ba2c68ea61f7a54a6ecfba60ef691a
orbitalno11 <sitthinonit@gmail.com>
2021-02-06 18:06:11 +0700

    chore: 🔧 add tutor route

--- routes/v1/Tutor.ts
+++ routes/v1/Tutor.ts
@@ -16,6 +16,10 @@ class TutorRouter extends Route {
         const uploader2MB = uploadMiddleware.uploadImage2Mb("tutor")
 
         this.router.route("/create").post(uploader2MB, (req, res, next) => controllerHandler(this.controller.create(req, res, next)))
+        this.router.route("/:id")
+            .get((req, res, next) => controllerHandler(this.controller.read(req, res, next)))
+            .put(uploader2MB, (req, res, next) => controllerHandler(this.controller.update(req, res, next)))
+            .delete((req, res, next) => controllerHandler(this.controller.delete(req, res, next)))
     }
 }
 
