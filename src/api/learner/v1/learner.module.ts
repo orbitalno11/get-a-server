fdf2dbaed6a3033ee3f41b836b198fda9a9ec5c3
Sitthinon S <sitthinonit@gmail.com>
2021-04-03 15:03:52 +0700

    Fix auth route #24

--- src/api/learner/v1/learner.module.ts
+++ src/api/learner/v1/learner.module.ts
@@ -1,21 +1,22 @@
-import { MiddlewareConsumer, Module, NestModule, RequestMethod } from "@nestjs/common"
-import { CoreModule } from "../../../core/core.module"
-import { UtilityModule } from "../../../utils/utility.module"
-import { LearnerController } from "./learner.controller"
-import { LearnerService } from "./learner.service"
+import {MiddlewareConsumer, Module, NestModule, RequestMethod} from "@nestjs/common"
+import {CoreModule} from "../../../core/core.module"
+import {UtilityModule} from "../../../utils/utility.module"
+import {LearnerController} from "./learner.controller"
+import {LearnerService} from "./learner.service"
 import Authenticated from "../../../middleware/auth/Authenticated.middleware"
 
 @Module({
-  imports: [CoreModule, UtilityModule],
-  controllers: [LearnerController],
-  providers: [LearnerService]
+    imports: [CoreModule, UtilityModule],
+    controllers: [LearnerController],
+    providers: [LearnerService]
 })
 export class LearnerModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(Authenticated)
-      .forRoutes(
-        { path: "v1/learner/:id", method: RequestMethod.ALL }
-      )
-  }
+    configure(consumer: MiddlewareConsumer) {
+        consumer
+            .apply(Authenticated)
+            .exclude("v1/learner/create")
+            .forRoutes(
+                {path: "v1/learner/:id", method: RequestMethod.ALL}
+            )
+    }
 }
