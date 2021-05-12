eb958b2f0b4002b6b25a7d25b7071e6c93da95b6
orbitalno11 <sitthinonit@gmail.com>
2021-05-12 11:40:43 +0700

    validate review form

--- src/api/review/v1/review.controller.ts
+++ src/api/review/v1/review.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, UseFilters, UseInterceptors } from "@nestjs/common"
+import { Body, Controller, HttpStatus, Post, UseFilters, UseInterceptors } from "@nestjs/common"
 import { ReviewService } from "./review.service"
 import { FailureResponseExceptionFilter } from "../../../core/exceptions/filters/FailureResponseException.filter"
 import { ErrorExceptionFilter } from "../../../core/exceptions/filters/ErrorException.filter"
@@ -9,6 +9,10 @@ import User from "../../../model/User"
 import { launch } from "../../../core/common/launch"
 import IResponse from "../../../core/response/IResponse"
 import SuccessResponse from "../../../core/response/SuccessResponse"
+import ReviewFormValidator from "../../../utils/validator/review/ReviewFormValidator"
+import { logger } from "../../../core/logging/Logger"
+import FailureResponse from "../../../core/response/FailureResponse"
+import CommonError from "../../../core/exceptions/constants/common-error.enum"
 
 /**
  * Class for controller "v1/review"
@@ -29,7 +33,18 @@ export class ReviewController {
     @Post()
     createReview(@Body() body: ReviewForm, @CurrentUser() user: User): Promise<IResponse<string>> {
         return launch(async () => {
+            const data = ReviewForm.createFromBody(body)
+            const validator = new ReviewFormValidator()
+            validator.setData(data)
+            const validate = validator.validate()
+
+            if (!validate.valid) {
+                logger.error("Invalid data")
+                throw FailureResponse.create(CommonError.INVALID_REQUEST_DATA, HttpStatus.BAD_REQUEST, validate.error)
+            }
+
             await this.service.createReview(body, user)
+
             return SuccessResponse.create("Successful")
         })
     }
---
+++ src/utils/validator/review/ReviewFormValidator.ts
@@ -0,0 +1,45 @@
+import AbstractValidator from "../AbstractValidator"
+import ReviewForm from "../../../model/review/ReviewForm"
+import ValidateResult from "../ValidateResult"
+import { isEmpty } from "../../../core/extension/CommonExtension"
+
+/**
+ * Validator class for review form
+ * @author orbitalno11 2021 A.D.
+ */
+class ReviewFormValidator extends AbstractValidator<ReviewForm>{
+    validator(): ValidateResult<any> {
+        if (!this.form.id?.isSafeNotBlank()) {
+            this.errors["id"] = "id is invalid"
+        }
+
+        if (!this.form.comment?.isSafeNotBlank()) {
+            this.errors["comment"] = "comment is invalid"
+        }
+
+        if (this.form.rating?.isSafeNumber()) {
+            if (!this.form.rating?.isBetween(1, 5)) {
+                this.errors["rating"] = "rating is invalid"
+            }
+        } else {
+            this.errors["rating"] = "rating is required"
+        }
+
+        if (!(typeof this.form.isClip === "boolean")) {
+            this.errors["isClip"] = "value is invalid"
+        }
+
+        if (!(typeof this.form.isOfflineCourse === "boolean")) {
+            this.errors["isOfflineCourse"] = "value is invalid"
+        }
+
+        this.isValid = isEmpty(this.errors)
+
+        return {
+            error: this.errors,
+            valid: this.isValid
+        }
+    }
+}
+
+export default ReviewFormValidator
