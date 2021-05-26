d2473b75d255415b68a264c30ffcf700580ea38d
orbitalno11 <sitthinonit@gmail.com>
2021-05-26 18:51:23 +0700

    [Home] Get Online course rank

--- src/repository/HomeRepository.ts
+++ src/repository/HomeRepository.ts
@@ -4,6 +4,8 @@ import { logger } from "../core/logging/Logger"
 import { TutorStatisticEntity } from "../entity/analytic/TutorStatistic.entity"
 import ErrorExceptions from "../core/exceptions/ErrorExceptions"
 import { HomeError } from "../core/exceptions/constants/home-error.enum"
+import { ClipEntity } from "../entity/course/clip/Clip.entity"
+import { OnlineCourseEntity } from "../entity/course/online/OnlineCourse.entity"
 
 /**
  * Repository for home
@@ -35,6 +37,26 @@ class HomeRepository {
         } catch (error) {
             logger.error(error)
             throw ErrorExceptions.create("Can not get tutor rank", HomeError.CAN_NOT_GET_TUTOR_RANK)
+        }
+    }
+
+    /**
+     * Get online course by rank
+     * waiting for ranking sys. keep online course rank
+     * @param rankLimit
+     */
+    async getOnlineCourse(rankLimit: number = 10): Promise<OnlineCourseEntity[]> {
+        try {
+            return await this.connection.createQueryBuilder(OnlineCourseEntity, "course")
+                .leftJoinAndSelect("course.rating", "rating")
+                .leftJoinAndSelect("course.owner", "owner")
+                .leftJoinAndSelect("owner.member", "member")
+                .orderBy("rating.rating", "DESC")
+                .limit(rankLimit)
+                .getMany()
+        } catch (error) {
+            logger.error(error)
+            throw ErrorExceptions.create("Can not get online clip rank", HomeError.CAN_NOT_GET_ONLINE_COURSE_RANK)
         }
     }
 }
