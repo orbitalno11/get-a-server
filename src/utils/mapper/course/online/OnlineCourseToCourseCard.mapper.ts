fe1823736e3467a797697053826d96d5369c511c
orbitalno11 <sitthinonit@gmail.com>
2021-05-26 18:51:23 +0700

    [Home] Get Online course rank

--- src/utils/mapper/course/online/OnlineCourseToCourseCard.mapper.ts
+++ src/utils/mapper/course/online/OnlineCourseToCourseCard.mapper.ts
@@ -10,7 +10,7 @@ export class OnlineCourseToCourseCardMapper implements Mapper<OnlineCourseEntity
         card.name = from.name
         card.coverUrl = from.coverUrl
         card.rating = from.rating ? from.rating?.rating : 0
-        card.owner = from.owner ?TutorEntityToPublicProfileMapper.getTutorNameCard(from.owner) : undefined
+        card.owner = TutorEntityToPublicProfileMapper.getTutorNameCard(from.owner)
         card.numberOfVideo = 0 // TODO keep number of clip in course
         return card
     }
