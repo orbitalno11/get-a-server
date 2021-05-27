28da59fdb065a63908ce219c2361578d5c481fe6
orbitalno11 <sitthinonit@gmail.com>
2021-05-27 22:05:58 +0700

    [Bug][HotFix] Fix buy clip bug

--- src/model/coin/data/CoinTransaction.enum.ts
+++ src/model/coin/data/CoinTransaction.enum.ts
@@ -7,7 +7,13 @@ export enum CoinTransactionType {
     WITHDRAW = 1,
     TRANSFER = 2,
     PAID = 3,
-    REQUEST_REDEEM = 4,
-    WAITING_FOR_APPROVE = 5,
-    REQUEST_REDEEM_APPROVED = 6
+    INCOME =4
+}
+
+export enum CoinRedeemStatus {
+    REQUEST_REDEEM_SENT = 0,
+    REQUEST_REDEEM_APPROVED = 1,
+    REQUEST_REDEEM_TRANSFER = 2,
+    REQUEST_REDEEM_DENIED = 3,
+    REQUEST_REDEEM_CANCELED = 4
 }
