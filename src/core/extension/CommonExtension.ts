10632b9a3094ee60de0045da8dc4564c4dd3d115
orbitalno11 <sitthinonit@gmail.com>
2021-06-08 16:38:23 +0700

    [Bug][Coin] Create coin validation bug

--- src/core/extension/CommonExtension.ts
+++ src/core/extension/CommonExtension.ts
@@ -1,16 +1,18 @@
 // TODO refactor this to common.extemsion.ts like string.extension.ts
 
 const isEmpty = (data: any): boolean =>
-  data === null ||
-  data === undefined ||
-  (typeof data === 'object' && Object.keys(data).length === 0) ||
-  (typeof data === 'string' && data.isBlank());
+    data === null ||
+    data === undefined ||
+    (typeof data === "object" && Object.keys(data).length === 0) ||
+    (typeof data === "string" && data.isBlank())
 
-const isNotEmpty = (data: any): boolean => !isEmpty(data);
+const isNotEmpty = (data: any): boolean => !isEmpty(data)
 
-const isNotNull = (data: any): boolean => data !== null;
+const isNotNull = (data: any): boolean => data !== null
 
 const isSafeNotNull = (data: any): boolean =>
-  isNotNull(data) && data !== undefined;
+    isNotNull(data) && data !== undefined
 
-export { isEmpty, isNotEmpty, isNotNull, isSafeNotNull };
+const isValidDate = (data: Date): boolean => data instanceof Date && isNaN(data.getTime())
+
+export { isEmpty, isNotEmpty, isNotNull, isSafeNotNull, isValidDate }
